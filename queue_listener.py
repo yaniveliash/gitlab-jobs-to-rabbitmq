42da877a6c2e8fcaad49c4cceb7455db9ca3bf39
Yaniv Eliash <yaniv@sre.engineer>
2023-03-13 16:59:35 +0200

    Simplified listener
    Signed-off-by: Yaniv Eliash <yaniv@sre.engineer>

--- queue_listener.py
+++ queue_listener.py
@@ -1,9 +1,6 @@
-from tools import connect
 from tools.get import getMessage
 import sys
 import signal
-from tools.error import error
-from tools import cli
 
 
 def signal_handler(sig, frame):
@@ -13,20 +10,4 @@ def signal_handler(sig, frame):
 
 signal.signal(signal.SIGINT, signal_handler)
 
-args = cli.queue()
-
-try:
-    connection, channel = connect.rabbitmq(args.rabbit_user, args.rabbit_pass,
-                                           args.rabbit_host, args.rabbit_port,
-                                           args.rabbit_vhost)
-except Exception:
-    error('Error connecting to RabbitMQ')
-
-getMessage(channel, args)
-# try:
-#     getMessage(channel, args)
-# except Exception:
-#     error('Error getting messages from RabbitMQ')
-
-# Close the connection
-connection.close()
+getMessage()
